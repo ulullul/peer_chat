d526707a4a271fe740f2780a195ce8d933a673a9
Andrii Prytula <a.prytula@lampalampa.net>
2022-12-13 02:33:57 +0200

    - fixed rest files

--- android/app/src/main/kotlin/com/mkr/peer_chat/peer_chat/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mkr.peer_chat.peer_chat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/app/data/exceptions/key_seed.exception.dart
+++
@@ -1,3 +0,0 @@
-class KeySeedException implements Exception {
-  
-}
